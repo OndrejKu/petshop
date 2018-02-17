87dd2aea309af2e8383ca60a5639191a193b67d5
Ondrej Kubala <kub.ond@gmail.com>
2018-02-17 19:22:38 -0200

    Customizes the behaviour of sell after save and sets total.

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -11,11 +11,28 @@ class Sell < ApplicationRecord
   has_many :sell_services
   has_many :services, through: :sell_services
 
+  before_save :set_total
+
   def fae_display_field
     id
   end
 
   def self.for_fae_index
     order(:id)
+  end
+
+  private
+
+  def set_total
+    total = 0
+
+    self.products.each { |p| total += p.price }
+    self.services.each { |s| total += s.price }
+
+    total -= self.discount.value if self.discount.present?
+
+    total = total >= 0 ? total : 0
+
+    self.total = total
   end
 end
